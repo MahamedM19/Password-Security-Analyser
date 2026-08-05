a7ace68b9667b55bbdb046335fcf968296af8ef4
MahamedM19 <mahamedmohamed5675@gmail.com>
2026-08-05 23:29:02 +0100

    Enhance password analysis with character checks
    added a check for lowercase, uppercase, numbers and special by using the import string.

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,7 +1,15 @@
+import string
+
+
 print("----- The BEST password security analsyer in the world -----")
 
 password = input("Please Enter your password and I shall analyse it for you: ")
 password_len = len(password)
+
+lowercase = any(char in string.ascii_lowercase for char in password)
+uppercase = any(char in string.ascii_uppercase for char in password)
+numbers = any(char in string.digits for char in password)
+special = any(char in string.punctuation for char in password)
 
 
 print("Your password is", password)
@@ -11,3 +19,9 @@ if password_len < 5:
     print("Your password is way too short")
 else:
     print("Your password is a good length!")
+
+print("/nAnalysis:")
+print("lowercase:", lowercase)
+print("uppercase:", uppercase)
+print("numbers:", numbers)
+print("special", special)
