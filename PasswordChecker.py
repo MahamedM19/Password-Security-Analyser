e001ec4904d879af70e499e1dd77dcd4e1cdca1e
MahamedM19 <mahamedmohamed5675@gmail.com>
2026-08-05 23:16:09 +0100

    Add password length analysis to PasswordChecker

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,5 +1,13 @@
 print("----- The BEST password security analsyer in the world -----")
 
 password = input("Please Enter your password and I shall analyse it for you: ")
+password_len = len(password)
 
-print("Your password is", password) 
+
+print("Your password is", password)
+print("Your password length is", password_len)
+
+if password_len < 5:
+    print("Your password is way too short")
+else:
+    print("Your password is a good length!")
